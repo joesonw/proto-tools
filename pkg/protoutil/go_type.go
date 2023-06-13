24114a6e9bd0c695ad7244c31e372560ce3000e3
Qiaosen Huang <joesonw@gmail.com>
2023-06-13 20:59:57 +0800

    fix FieldGoType logic

--- pkg/protoutil/go_type.go
+++ pkg/protoutil/go_type.go
@@ -9,17 +9,10 @@ import (
 
 type Q func(protogen.GoIdent) string
 
-type Option func(q Q, f *protogen.Field) (ok bool, result string)
+type Option func(q Q, f *protogen.Field) (skip bool, result string)
 
 func FieldGoType(q Q, f *protogen.Field, options ...Option) string {
 	goType := ""
-	ok := true
-	for _, option := range options {
-		ok, goType = option(q, f)
-	}
-	if !ok {
-		return goType
-	}
 
 	if f.Desc.IsMap() {
 		keyType := FieldGoType(q, f.Message.Fields[0], options...)
@@ -27,29 +20,35 @@ func FieldGoType(q Q, f *protogen.Field, options ...Option) string {
 		return fmt.Sprintf("map[%s]%s", keyType, valueType)
 	}
 
-	switch f.Desc.Kind() {
-	case protoreflect.BoolKind:
-		goType = "bool"
-	case protoreflect.StringKind:
-		goType = "string"
-	case protoreflect.Int32Kind, protoreflect.Sint32Kind, protoreflect.Sfixed32Kind:
-		goType = "int32"
-	case protoreflect.Uint32Kind, protoreflect.Fixed32Kind:
-		goType = "uint32"
-	case protoreflect.Int64Kind, protoreflect.Sint64Kind, protoreflect.Sfixed64Kind:
-		goType = "int64"
-	case protoreflect.Uint64Kind, protoreflect.Fixed64Kind:
-		goType = "uint64"
-	case protoreflect.FloatKind:
-		goType = "float32"
-	case protoreflect.DoubleKind:
-		goType = "float64"
-	case protoreflect.BytesKind:
-		goType = "[]byte"
-	case protoreflect.EnumKind:
-		goType = q(f.Enum.GoIdent)
-	case protoreflect.MessageKind, protoreflect.GroupKind:
-		goType = "*" + q(f.Message.GoIdent)
+	skip := false
+	for _, option := range options {
+		skip, goType = option(q, f)
+	}
+	if !skip {
+		switch f.Desc.Kind() {
+		case protoreflect.BoolKind:
+			goType = "bool"
+		case protoreflect.StringKind:
+			goType = "string"
+		case protoreflect.Int32Kind, protoreflect.Sint32Kind, protoreflect.Sfixed32Kind:
+			goType = "int32"
+		case protoreflect.Uint32Kind, protoreflect.Fixed32Kind:
+			goType = "uint32"
+		case protoreflect.Int64Kind, protoreflect.Sint64Kind, protoreflect.Sfixed64Kind:
+			goType = "int64"
+		case protoreflect.Uint64Kind, protoreflect.Fixed64Kind:
+			goType = "uint64"
+		case protoreflect.FloatKind:
+			goType = "float32"
+		case protoreflect.DoubleKind:
+			goType = "float64"
+		case protoreflect.BytesKind:
+			goType = "[]byte"
+		case protoreflect.EnumKind:
+			goType = q(f.Enum.GoIdent)
+		case protoreflect.MessageKind, protoreflect.GroupKind:
+			goType = "*" + q(f.Message.GoIdent)
+		}
 	}
 
 	if f.Desc.IsList() {
